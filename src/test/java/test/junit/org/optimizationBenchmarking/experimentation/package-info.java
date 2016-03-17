9397ed72e9aff25b65a7c38481d3e57963a5b8c3
Thomas Weise <tweise@ustc.edu.cn>
2016-03-17 09:42:15 +0800

    Minor Change in Documentation

--- src/test/java/test/junit/org/optimizationBenchmarking/experimentation/package-info.java
+++ src/test/java/test/junit/org/optimizationBenchmarking/experimentation/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Tests of our
- * {@link test.junit.org.optimizationBenchmarking.experimentation
+ * {@link test.junit.org.optimizationBenchmarking.evaluator
  * Experiment Tools}.
  */
 package test.junit.org.optimizationBenchmarking.experimentation;
